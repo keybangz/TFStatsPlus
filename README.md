b0af32b8b1ac8e7fcd8f075255ad74fa731bb9fc
keybangz <wyatt.wheelerr@gmail.com>
2023-08-15 17:32:02 +1000

    fix refresh

--- README.md
+++
@@ -1,2 +0,0 @@
-# TF2_Dodgeball_Stats
-A Stat plugin mainly used for the dodgeball gamemode of Team Fortress 2. Soon to have a web leaderboard
--- WebPanel/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{vue,js,ts}'],
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: ["night"],
-  },
-};
